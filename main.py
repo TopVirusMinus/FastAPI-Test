96afb2ca0856e32cf26d6a76ca1f5fe62b47991e
TopVirusMinus <mwmma5000@gmail.com>
2022-10-06 11:36:32 +0200

    added post functionality

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from fastapi import FastAPI
+import uuid
+
+
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "saba7 el gamal"}
+logs = [
+        {"id":"5d58449d-092f-4096-8f78-a810d0179653", "message": "saba7 el gamal"}
+    ]
+
+@app.get("/getLogs")
+async def getLogs():
+    return logs
+
+@app.get("/getLogs/{log_id}")
+async def getLog(log_id: str):
+    for log in logs:
+        if log.get("id") == log_id:
+            return log  
+        
+@app.post("/addLog")
+async def addLog(message):
+    newLog = {}
+    newLog["id"] = uuid.uuid4()
+    newLog["message"] = message
+    
+    logs.append(newLog)
+    return newLog
+
